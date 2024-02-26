c57348f53c50484b0a65b948bdfa19df70916977
Harsh-dev-cyno <dev.in1@cyno.co.in>
2024-02-26 14:13:49 +0530

    array_traversing

---
+++ array_traversing.js
@@ -0,0 +1,10 @@
+// Array traversing and accessing
+let data = [9,45,85,78,65,34,67,81];
+let x = 2;
+// Array accessing
+console.log(`Array ${x} Element is ${data[x]}`);
+
+// Array traversing
+for (let i = 0; i < data.length; i++) {
+console.log(`Array ${i} is ${data[i]}`);
+}
