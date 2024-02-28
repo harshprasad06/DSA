15867517861fc353da23f54eb7445296bf15ebd2
Harsh-dev-cyno <dev.in1@cyno.co.in>
2024-02-28 11:25:40 +0530

    update queue add front and rear find function

--- queue.js
+++ queue.js
@@ -12,7 +12,7 @@ function enqueue(value) {
 }
 
 function dequeue() {
-  if (currentSize > 0) {
+  if (isEmpty()) {
     for (let i = 0; i < queue.length; i++) {
       // const element f= array[i];
       queue[i] = queue[i + 1];
@@ -23,19 +23,34 @@ function dequeue() {
     console.log("queue is empty");
   }
 }
+function front() {
+  if (isEmpty()) {
+    console.log("front", queue[0]);
+  } else {
+    console.log("queue is empty");
+  }
+}
+function rear() {
+  if (isEmpty()) {
+    console.log("rear", queue[currentSize - 1]);
+  } else {
+    console.log("queue is empty");
+  }
+}
+function isEmpty() {
+  if (currentSize > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 enqueue(45);
 enqueue(6);
 enqueue(12);
-enqueue(2);
-enqueue(2);
-
-enqueue(2);
-
-dequeue();
-dequeue();
-dequeue();
 dequeue();
 dequeue();
 dequeue();
 
-console.log(queue);
+// console.log(queue);
+front();
+rear();
